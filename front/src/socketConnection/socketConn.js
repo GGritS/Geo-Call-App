d490b08245f1eacef702e2a57ce132c6e8234520
Ivan Hrytsenko <ivanhrytsenko23143@gmail.com>
2023-05-25 11:47:35 +0300

    changed socket.io connection path in fronted

--- front/src/socketConnection/socketConn.js
+++ front/src/socketConnection/socketConn.js
@@ -10,7 +10,7 @@ import { call, disconnect } from "../realtimeCommunication/webRTCHandler";
 let socket = null;
 
 export const connectWithSocketIoServer = () => {
-  socket = io("http://localhost:3003");
+  socket = io("https://geo-call-app-api.vercel.app");
   socket.on("connect", () => {
     console.log("connected to socket server");
   });
